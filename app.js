9e1c5ecebb841d6e1545957f4a511f6591eeb992
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-11-16 13:59:47 -0800

    Fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 app.use(express.json());
-const port = 11;
+const port = process.env.PORT || 3011;
 
 const uri =
   "mongodb+srv://afordm99:VAbiGK7nx26VILb3@heatgame.qos8kpx.mongodb.net/?retryWrites=true&w=majority";
